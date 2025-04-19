a2984fcc2c3e3481abed1008b8c934261288055c
yogendratamang48 <yogen48@hotmail.com>
2025-04-19 12:32:21 +0200

    setter injection

--- store/src/main/java/com/ytamang/store/OrderService.java
+++ store/src/main/java/com/ytamang/store/OrderService.java
@@ -3,12 +3,18 @@ package com.ytamang.store;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
+    // Constructor
+    // public OrderService(PaymentService paymentService) {
+    //     this.paymentService = paymentService;
 
-    }
+    // }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+    
+    // setter
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
