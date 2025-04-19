cb4f41d24c5147df49d26d68a8f62e126adb8d6e
yogendratamang48 <yogen48@hotmail.com>
2025-04-19 13:38:16 +0200

    add annotations with beans

--- store/src/main/java/com/ytamang/store/PaypalPaymentService.java
+++ store/src/main/java/com/ytamang/store/PaypalPaymentService.java
@@ -1,5 +1,9 @@
 package com.ytamang.store;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service("paypal")
 public class PaypalPaymentService implements PaymentService{
     @Override
     public void processPayment(double amount) {
