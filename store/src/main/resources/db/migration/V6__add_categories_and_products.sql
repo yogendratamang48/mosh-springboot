43ec30c99cc508bd942455a3263ab8a016086267
yogendratamang48 <yogen48@hotmail.com>
2025-04-21 21:19:19 +0200

    add flyway migration

---
+++ store/src/main/resources/db/migration/V6__add_categories_and_products.sql
@@ -0,0 +1,15 @@
+-- store.categories definition
+
+CREATE TABLE `categories` (
+  `id` tinyint PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255)
+);
+
+-- store.products
+CREATE TABLE `products` (
+  `bigint` BIGINT PRIMARY KEY AUTO_INCREMENT,,
+  `name` VARCHAR(255),
+  `price` DECIMAL(10, 2),
+  `category_id` TINYINT,
+  FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+);
