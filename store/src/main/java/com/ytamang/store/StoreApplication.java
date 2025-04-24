54f8d5ec002c3cd13fccc64d92615f7d495e794b
yogendratamang48 <yogen48@hotmail.com>
2025-04-24 21:44:11 +0200

    add save and delete information

--- store/src/main/java/com/ytamang/store/StoreApplication.java
+++ store/src/main/java/com/ytamang/store/StoreApplication.java
@@ -1,27 +1,26 @@
 package com.ytamang.store;
-// import org.springframework.context.ApplicationContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.ytamang.store.entities.Address;
-import com.ytamang.store.entities.Profile;
-import com.ytamang.store.entities.Tag;
-import com.ytamang.store.entities.User;
 import com.ytamang.store.repositories.UserRepository;
 
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var repository = context.getBean(UserRepository.class);
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var repository = context.getBean(UserRepository.class);
+//        var user = User.builder().name("Yogendra Tamang").email("yt@gmail.com").password("password").build();
+//
+//        repository.save(user);
 
 		var user =  repository.findById(1L).orElseThrow();
 		System.out.println(user.getEmail());
 		repository.deleteById(1L);
 
-		
-	}
+
+    }
 
 }
